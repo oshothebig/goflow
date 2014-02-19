c58b9754d479ccc447c69df5c1f339f32ab6a788
Sho SHIMIZU <sho.shimizu@gmail.com>
2014-02-20 08:42:18 +0900

    Declare constant related to port state

--- openflow/of10/port.go
+++ openflow/of10/port.go
@@ -48,3 +48,28 @@ var PortConfigs = struct {
 	OFPPC_NO_FWD,
 	OFPPC_NO_PACKET_IN,
 }
+
+const (
+	OFPPS_LINK_DOWN   PortState = 1 << 0
+	OFPPS_STP_LISTEN  PortState = 0 << 8
+	OFPPS_STP_LEARN   PortState = 1 << 8
+	OFPPS_STP_FORWARD PortState = 2 << 8
+	OFPPS_STP_BLOCK   PortState = 3 << 8
+	OFPPS_STP_MASK    PortState = 3 << 8
+)
+
+var PortStates = struct {
+	LinkDown   PortState
+	StpListen  PortState
+	StpLearn   PortState
+	StpForward PortState
+	StpBlock   PortState
+	StpMask    PortState
+}{
+	OFPPS_LINK_DOWN,
+	OFPPS_STP_LISTEN,
+	OFPPS_STP_LEARN,
+	OFPPS_STP_FORWARD,
+	OFPPS_STP_BLOCK,
+	OFPPS_STP_MASK,
+}
