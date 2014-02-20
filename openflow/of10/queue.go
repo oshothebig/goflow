b55d9b1565375162e2d64739b09447e5f7d60e49
Sho SHIMIZU <sho.shimizu@gmail.com>
2014-02-20 12:16:08 +0900

    Declare constant related to queue

---
+++ openflow/of10/queue.go
@@ -0,0 +1,46 @@
+package of10
+
+import . "github.com/oshothebig/goflow/openflow"
+
+type PacketQueue struct {
+	QueueId    uint32
+	Length     uint16
+	pad        [2]uint8
+	Properties []QueueProperty
+}
+
+type QueueProperty interface {
+	Packetizable
+	Header() *QueuePropertyHeader
+}
+
+type QueuePropertyType uint16
+
+const (
+	OFPQT_NONE QueuePropertyType = iota
+	OFPQT_MIN_RATE
+)
+
+var QueuePropertyTypes = struct {
+	None    QueuePropertyType
+	MinRate QueuePropertyType
+}{
+	OFPQT_NONE,
+	OFPQT_MIN_RATE,
+}
+
+type QueuePropertyHeader struct {
+	Property QueuePropertyType
+	Length   uint16
+	pad      [4]uint8
+}
+
+func (h *QueuePropertyHeader) Header() *QueuePropertyHeader {
+	return h
+}
+
+type QueuePropertyMinRate struct {
+	QueuePropertyHeader
+	Rate uint16
+	pad  [6]uint8
+}
