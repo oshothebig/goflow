0994a2bdb5cbccb33679dc11100d893b0bb0971b
Sho SHIMIZU <sho.shimizu@gmail.com>
2014-02-26 08:44:45 +0900

    Define FlowMod struct

--- openflow/of10/message.go
+++ openflow/of10/message.go
@@ -77,3 +77,60 @@ var ConfigFlags = struct {
 	OFPC_FRAG_REASM,
 	OFPC_FRAG_MASK,
 }
+
+type FlowMod struct {
+	Header
+	Match       Match
+	Cookie      Cookie
+	Command     FlowModCommand
+	IdleTimeout uint16
+	HardTimeout uint16
+	Priority    uint16
+	BufferId    BufferId
+	OutPort     PortNumber
+	Flags       FlowModFlag
+	Actions     []Action
+}
+
+type Cookie uint64
+type FlowModCommand uint16
+type BufferId uint32
+type FlowModFlag uint16
+
+const (
+	OFPFC_ADD FlowModCommand = iota
+	OFPFC_MODIFY
+	OFPFC_MODIFY_STRICT
+	OFPFC_DELETE
+	OFPFC_DELETE_STRICT
+)
+
+var FlowModCommands = struct {
+	Add          FlowModCommand
+	Modify       FlowModCommand
+	ModifyStrict FlowModCommand
+	Delete       FlowModCommand
+	DeleteStrict FlowModCommand
+}{
+	OFPFC_ADD,
+	OFPFC_MODIFY,
+	OFPFC_MODIFY_STRICT,
+	OFPFC_DELETE,
+	OFPFC_DELETE_STRICT,
+}
+
+const (
+	OFPFF_SEND_FLOW_REM FlowModFlag = 1 << iota
+	OFPFF_CHECK_OVERLAP
+	OFPFF_EMERG
+)
+
+var FlowModFlags = struct {
+	SendFlowRemoved FlowModFlag
+	CheckOverlap    FlowModFlag
+	Emergency       FlowModFlag
+}{
+	OFPFF_SEND_FLOW_REM,
+	OFPFF_CHECK_OVERLAP,
+	OFPFF_EMERG,
+}
