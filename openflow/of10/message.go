a0a5d4105bd5d7c4158d663628ccdb160b86641a
Sho SHIMIZU <sho.shimizu@gmail.com>
2014-03-01 18:00:25 +0900

    Define ErrorMessage struct

--- openflow/of10/message.go
+++ openflow/of10/message.go
@@ -399,3 +399,77 @@ var PortStatusReasons = struct {
 	OFPPR_DELETE,
 	OFPPR_MODIFY,
 }
+
+type ErrorMessage struct {
+	Header
+	Type ErrorType
+	Code ErrorCode
+	Data []uint8
+}
+
+type ErrorType uint16
+
+const (
+	OFPET_HELLO_FAILED ErrorType = iota
+	OFPET_BAD_REQUEST
+	OFPET_BAD_ACTION
+	OFPET_FLOW_MOD_FAILED
+	OFPET_PORT_MOD_FAILED
+	OFPET_QUEUE_OP_FAILED
+)
+
+type ErrorCode uint16
+
+// ErrorCode for Hello Failed
+const (
+	OFPHFC_INCOMPATIBLE ErrorCode = iota
+	OFPHFC_EPERM
+)
+
+// ErrorCode for Bad Request
+const (
+	OFPBRC_BAD_VERSION ErrorCode = iota
+	OFPBRC_BAD_TYPE
+	OFPBRC_BAD_STAT
+	OFPBRC_BAD_VENDOR
+	OFPBRC_BAD_SUBTYPE
+	OFPBRC_EPERM
+	OFPBRC_BAD_LEN
+	OFPBRC_BUFFER_EMPTY
+	OFPBRC_BUFFER_UNKNOWN
+)
+
+// ErrorCode for Bad Action
+const (
+	OFPBAC_BAD_TYPE ErrorCode = iota
+	OFPBAC_BAD_LEN
+	OFPBAC_BAD_VENDOR
+	OFPBAC_BAD_VENDOR_TYPE
+	OFPBAC_BAD_OUT_PORT
+	OFPBAC_BAD_ARGUMENT
+	OFPBAC_EPERM
+	OFPBAC_TOO_MANY
+	OFPBAC_BAD_QUEUE
+)
+
+// ErrorCode for Flow Mod Failed
+const (
+	OFPFMFC_ALL_TABLES_FULL ErrorCode = iota
+	OFPFMFC_OVERLAP
+	OFPFMFC_EPERM
+	OFPFMFC_BAD_EMERG_TIMEOUT
+	OFPFMFC_BAD_COMMAND
+	OFPFMFC_UNSUPPORTED
+)
+
+// ErrorCode for Port Mod Failed
+const (
+	OFPPMFC_BAD_PORT ErrorCode = iota
+	OFPPMFC_BAD_HW_ADDR
+)
+
+// ErrorCode for Queue Op Failed
+const (
+	OFPQOFC_BAD_QUEUE ErrorCode = iota
+	OFPQOFC_EPERM
+)
